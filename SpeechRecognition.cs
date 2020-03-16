daec2a387037dafc10f5237cd48e7784eef856c9
Mikeul <56322741+Mikeultron@users.noreply.github.com>
2020-03-17 06:16:26 +0700

    Update and rename SpeechReconition.cs to SpeechRecognition.cs

--- SpeechRecognition.cs
+++ SpeechRecognition.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Windows.Speech;
 using System.Linq;
 
-public class SpeechReconition : MonoBehaviour
+public class SpeechRecognition : MonoBehaviour
 {
     string command = "stop";
     KeywordRecognizer kwR;
